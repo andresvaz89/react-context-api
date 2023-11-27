8bfc67eac333f3265066e1a2bcf61a6d44fccc58
Andres Vazquez <andres.vazquez.martin@gmail.com>
2023-11-27 14:29:33 +0100

    useContext succesfully added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from 'react-router-dom';
+import { useContext } from 'react';
+import { ThemeContext } from '../context/theme.context';
+
+function Navbar() {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  return (
+    <nav className={'Navbar ' + theme}>
+      <div>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/projects">Projects</NavLink>
+      </div>
+
+      {/*    ADD     */}
+      <button className="theme-btn" onClick={toggleTheme}>
+        {theme === 'light' ? 'dark ' : 'light '}
+      </button>
+    </nav>
+  );
+}
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { ThemeProviderWrapper } from './context/theme.context';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <ThemeProviderWrapper>
+        <App />
+      </ThemeProviderWrapper>
+    </Router>
+  </React.StrictMode>
+);
